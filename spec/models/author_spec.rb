c9c270ff393d2706724b63ccad81ed0a38df80dc
Konstantin Ketterer <konstantin.ketterer@d-labs.com>
2022-11-14 16:20:06 +0100

    Test for author model

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
+  it "should have a first_name, last_name and homepage" do
+    author = Author.new(first_name:"Jürgen",last_name:"Günter", homepage: "https://hpi.de")
+    expect(author.first_name).to eq("Jürgen")
+    expect(author.last_name).to eq("Günter")
+    expect(author.homepage).to eq("https://hpi.de")
+  end
   it "should have a method name (no parameters) which returns the full name of an author" do
     author = Author.new(first_name:"Jürgen",last_name:"Günter")
     expect(author.name()).to eq("Jürgen Günter")
