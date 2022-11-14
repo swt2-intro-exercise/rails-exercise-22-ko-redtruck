b421ad1e690ee672e9bf47cc77dcc63c5d410f5c
Konstantin Ketterer <konstantin.ketterer@d-labs.com>
2022-11-14 22:02:16 +0100

    Fix: #13

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not validate without a title" do
+    paper = Paper.new(year:2000)
+    expect(paper).to_not be_valid
+  end
+
 end
