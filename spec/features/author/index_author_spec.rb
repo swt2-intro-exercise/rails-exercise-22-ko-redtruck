231662d81f72624db0ff2a31f5271e7740671db8
Konstantin Ketterer <konstantin.ketterer@d-labs.com>
2022-11-14 21:09:13 +0100

    Fix: #10

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -4,8 +4,14 @@ describe "Authors page", :type => :feature do
   it "should show an overview page of all saved authors'" do
     visit authors_path
     expect(page).to have_link 'New', href: new_author_path
+    
   end
   it "should link to the authors pages" do
     visit authors_path
+  end
+  it "should have a link to edit the authors" do
+    alan = FactoryBot.create :author
+    visit authors_path
+    expect(page).to have_link 'Edit', href: edit_author_path(alan)
   end
 end
